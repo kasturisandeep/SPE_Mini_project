688b4b70d8e86b10aa5593df4746037fad585029
Kasturi Sandeep <kasturisandeep9@gmail.com>
2023-03-20 10:55:25 +0530

    added docker correctly

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,15 +1,35 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-
 public class CalculatorTest {
     Calculator obj = new Calculator();
     @Test
-    public void calctest(){
-        assertEquals("Finding squareroot of a number", 8, obj.squareroot_func(64), 0.00001 );
-        assertEquals("Finding factorial of a number", 720, obj.factorial_func(6), 0.00001 );
-        assertEquals("Finding power of a number", 64, obj.power_func(8,2), 0.00001 );
-        assertEquals("Finding logarithm of a number", 1.09861228867, obj.log_func(3), 0.00001 );
+    public void sqaureroot_test_tp(){
 
+        assertEquals("Finding squareroot of a number(TP)", 8, obj.squareroot_func(64), 0.00001 );
+        assertEquals("Finding squareroot of a number(TP)", 5, obj.squareroot_func(25), 0.00001 );
+    }
+
+    @Test
+    public void factorial_test_tp()
+    {
+        assertEquals("Finding factorial of a number(TP)", 720, obj.factorial_func(6), 0.00001 );
+        assertEquals("Finding factorial of a number(TP)", 24, obj.factorial_func(4), 0.00001 );
+    }
+//
+//
+    @Test
+    public void logarth_test_tp()
+    {
+        assertEquals("Finding logarithm of a number(tp)", 1.09861228867, obj.log_func(3), 0.00001 );
+        assertEquals("Finding logarithm of a number(tp)", 0.69314718056, obj.log_func(2), 0.00001 );
+    }
+//
+//
+    @Test
+    public void power_test_tp()
+    {
+        assertEquals("Finding power of a number(tp)", 64, obj.power_func(8,2), 0.00001 );
+        assertEquals("Finding power of a number(tp)", 32, obj.power_func(2,5), 0.00001 );
     }
 
 
